48968e44ea2ba364088a74e8ed2c6e607674a0d6
Nicolas Skuček <nico.skucek@gmail.com>
2024-09-11 12:02:51 +0200

    Added migrations for user entities

--- src/ServiceTrack.Api/Program.cs
+++ src/ServiceTrack.Api/Program.cs
@@ -1,8 +1,28 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ServiceTrack.Data;
+using ServiceTrack.Data.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetValue<string>("ConnectionStrings:DbConnection"));
+});
 
 builder.Services.AddControllers();
+builder.Services.AddIdentity<User, Role>(options =>
+    {
+        options.User.RequireUniqueEmail = true;
+        options.Tokens.PasswordResetTokenProvider = TokenOptions.DefaultEmailProvider;
+    })
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- src/ServiceTrack.Data/AppDbContext.cs
+++ src/ServiceTrack.Data/AppDbContext.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ServiceTrack.Data.Entities;
 
 namespace ServiceTrack.Data;
 
 public class AppDbContext : IdentityDbContext<User, Role, Guid, IdentityUserClaim<Guid>, UserRole, IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(e => e.Role)
+            .WithMany(e => e.Users)
+            .HasForeignKey(e => e.RoleId)
+            .HasPrincipalKey(e => e.Id);
+        modelBuilder.Entity<UserRole>()
+            .HasOne(e => e.User)
+            .WithMany(e => e.Roles)
+            .HasForeignKey(e => e.UserId)
+            .HasPrincipalKey(e => e.Id);
+    }
 }
