9d29e8cf40f98e4adfae02a7f1535082f8a76d77
Nicolas Skuček <nico.skucek@gmail.com>
2024-11-02 16:23:29 +0100

    Added comment to auth controller

--- src/ServiceTrack.Api/Controllers/AuthController.cs
+++ src/ServiceTrack.Api/Controllers/AuthController.cs
@@ -10,10 +10,15 @@ namespace ServiceTrack.Api.Controllers;
 [ApiController]
 public class AuthController(IMediator mediator) : Controller
 {
+    /// <summary>
+    /// Logs in the user with the given information
+    /// </summary>
+    /// <param name="loginDto">Email and password to login user</param>
+    /// <returns></returns>
     [HttpPost("api/v1/Auth/Login")]
-    public async Task<ActionResult> Login([FromBody] LoginDto model)
+    public async Task<ActionResult> Login([FromBody] LoginDto loginDto)
     {
-        var generateClaimsPrincipalForUserCommand = new GenerateClaimsPrincipalForUserCommand(model);
+        var generateClaimsPrincipalForUserCommand = new GenerateClaimsPrincipalForUserCommand(loginDto);
         var userPrincipal = await mediator.Send(generateClaimsPrincipalForUserCommand);
 
         await HttpContext.SignInAsync(userPrincipal);
@@ -21,18 +26,23 @@ public class AuthController(IMediator mediator) : Controller
         return NoContent();
     }
 
+    /// <summary>
+    /// Registers a new user with the given information. The user is not logged in after registration
+    /// </summary>
+    /// <param name="registerUserDto">Information needed to create new user with specified password</param>
+    /// <returns></returns>
     [HttpPost("api/v1/Auth/Register")]
     public async Task<ActionResult> Register(
-        [FromBody] RegisterUserDto model
+        [FromBody] RegisterUserDto registerUserDto
     )
     {
-        var registerNewUserCommand = new RegisterNewUserCommand(model);
+        var registerNewUserCommand = new RegisterNewUserCommand(registerUserDto);
         await mediator.Send(registerNewUserCommand);
 
         var addPasswordToRegisteredUserCommand = new AddPasswordToRegisteredUserCommand(new AddPasswordToRegisteredUserDto
         {
-            Email = model.Email,
-            Password = model.Password
+            Email = registerUserDto.Email,
+            Password = registerUserDto.Password
         });
         await mediator.Send(addPasswordToRegisteredUserCommand);
 
@@ -42,7 +52,7 @@ public class AuthController(IMediator mediator) : Controller
     /// <summary>
     /// Generates a token for the user to confirm their email
     /// </summary>
-    /// <param name="email"></param>
+    /// <param name="email">Users email that specifies which user should get the token</param>
     /// <returns></returns>
     [HttpGet("api/v1/Auth/GenerateEmailConfirmToken")]
     public async Task<ActionResult<string>> GenerateToken([FromQuery] string email)
@@ -56,33 +66,33 @@ public class AuthController(IMediator mediator) : Controller
     /// <summary>
     /// Validates the token that the user has received in their email
     /// </summary>
-    /// <param name="model"></param>
+    /// <param name="confirmTokenForUserDto">Email and the given token for email confirmation</param>
     /// <returns></returns>
     [HttpPost("api/v1/Auth/ValidateToken")]
     public async Task<ActionResult> ValidateToken(
-        [FromBody] EmailConfirmTokenForUserDto model
+        [FromBody] EmailConfirmTokenForUserDto confirmTokenForUserDto
     )
     {
-        var validateEmailConfirmationTokenForUserCommand = new ValidateEmailConfirmationTokenCommand(model);
+        var validateEmailConfirmationTokenForUserCommand = new ValidateEmailConfirmationTokenCommand(confirmTokenForUserDto);
         await mediator.Send(validateEmailConfirmationTokenForUserCommand);
 
         return NoContent();
     }
 
+    //TODO: Move to a different controller
     /// <summary>
     /// Creates a user with the given information
     /// </summary>
-    /// <param name="model"></param>
+    /// <param name="createNewUserDto">Information that admin provides</param>
     /// <returns></returns>
     [HttpPost("api/v1/Auth/CreateUser")]
     public async Task<ActionResult> CreateUser(
-        [FromBody] CreateNewUserDto model
+        [FromBody] CreateNewUserDto createNewUserDto
     )
     {
-        var createNewUserCommand = new CreateNewUserCommand(model);
+        var createNewUserCommand = new CreateNewUserCommand(createNewUserDto);
         await mediator.Send(createNewUserCommand);
 
         return NoContent();
     }
 }
-
