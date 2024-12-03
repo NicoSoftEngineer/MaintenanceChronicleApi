bcaa1b6b5ee5e8d85c4bd812d1e0eb536dae6c5a
Nicolas Skuček <nico.skucek@gmail.com>
2024-12-03 10:21:23 +0100

    Added empty CustomerController

---
+++ src/ServiceTrack.Api/Controllers/CustomerController.cs
@@ -0,0 +1,10 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceTrack.Api.Controllers;
+
+[ApiController]
+public class CustomerController(IMediator mediator) : ControllerBase
+{
+    
+}
