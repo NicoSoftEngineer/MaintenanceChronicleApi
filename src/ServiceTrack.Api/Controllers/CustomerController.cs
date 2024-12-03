773f1c5cda9bfb5a17f9a8fa4fc48891897e712c
Nicolas Skuček <nico.skucek@gmail.com>
2024-12-03 14:59:10 +0100

    Added CreateCustomer endpoint and CreateNewCustomerCommand and its handler

--- src/ServiceTrack.Api/Controllers/CustomerController.cs
+++ src/ServiceTrack.Api/Controllers/CustomerController.cs
@@ -1,10 +1,37 @@
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ServiceTrack.Application.Contracts.Customers.Commands;
+using ServiceTrack.Application.Contracts.Customers.Commands.Dto;
+using ServiceTrack.Application.Contracts.Users.Commands.Dto;
+using ServiceTrack.Application.Contracts.Users.Commands;
+using ServiceTrack.Utilities.Helpers;
+using ServiceTrack.Utilities.Constants;
 
 namespace ServiceTrack.Api.Controllers;
 
+//Makes endpoints accessible only for users with Admin or GlobalAdmin roles
+[Authorize(Roles = $"{RoleTypes.Admin},{RoleTypes.GlobalAdmin}")]
 [ApiController]
 public class CustomerController(IMediator mediator) : ControllerBase
 {
-    
+    /// <summary>
+    /// Creates customer with the given information
+    /// </summary>
+    /// <param name="newCustomerDto">Information that admin provides</param>
+    /// <returns>New customer id</returns>
+    [HttpPost("api/v1/customers")]
+    public async Task<ActionResult<Guid>> CreateUser(
+        [FromBody] NewCustomerDto newCustomerDto
+    )
+    {
+        var createNewCustomerDto = new CreateNewCustomerCommand(
+            newCustomerDto,
+            User.GetUserId(),
+            User.GetTenantId()
+        );
+        var customerId = await mediator.Send(createNewCustomerDto);
+
+        return Ok(customerId);
+    }
 }
---
+++ src/ServiceTrack.Application/Customers/Commands/CreateNewCustomerCommandHandler.cs
@@ -0,0 +1,30 @@
+using MediatR;
+using NodaTime;
+using ServiceTrack.Application.Contracts.Customers.Commands;
+using ServiceTrack.Data;
+using ServiceTrack.Data.Entities.Business;
+using ServiceTrack.Data.Interfaces;
+
+namespace ServiceTrack.Application.Customers.Commands;
+
+public class CreateNewCustomerCommandHandler(AppDbContext dbContext, IClock clock) : IRequestHandler<CreateNewCustomerCommand, Guid>
+{
+    public async Task<Guid> Handle(CreateNewCustomerCommand request, CancellationToken cancellationToken)
+    {
+        //TODO: Add validation if user and tenant exist, and if user has access to tenant
+
+        var customerDto = request.NewCustomer;
+        var customer = new Customer
+        {
+            Id = Guid.NewGuid(),
+            Name = customerDto.Name,
+            TenantId = Guid.Parse(request.TenantId)
+        };
+        customer.SetCreateBy(request.UserId, clock.GetCurrentInstant());
+
+        await dbContext.Customers.AddAsync(customer, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return customer.Id;
+    }
+}
