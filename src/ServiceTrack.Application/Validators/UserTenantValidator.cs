5667f286e91dc08d42cbb1615da64521ef118f82
Nicolas Skuček <nico.skucek@gmail.com>
2024-12-13 14:52:37 +0100

    Added validator, that validates users access to tenant

---
+++ src/ServiceTrack.Application/Validators/UserTenantValidator.cs
@@ -0,0 +1,31 @@
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+using ServiceTrack.Data;
+
+namespace ServiceTrack.Application.Validators;
+
+public static class UserTenantValidator
+{
+    /// <summary>
+    /// Validates if user and tenant exist, and if user has access to tenant
+    /// </summary>
+    /// <param name="dbContext"></param>
+    /// <param name="userId"></param>
+    /// <param name="tenantId"></param>
+    /// <returns></returns>
+    public static async Task<bool> ValidateUserTenantAccess(this AppDbContext dbContext, string userId, string tenantId)
+    {
+        var user = await dbContext.Users.Include(x => x.Tenant).FirstOrDefaultAsync(u => u.Id == Guid.Parse(userId));
+        var tenant = await dbContext.Tenants.FirstOrDefaultAsync(t => t.Id == Guid.Parse(tenantId));
+
+        if (tenant != null && user != null)
+        {
+            if (user.TenantId == tenant.Id)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
