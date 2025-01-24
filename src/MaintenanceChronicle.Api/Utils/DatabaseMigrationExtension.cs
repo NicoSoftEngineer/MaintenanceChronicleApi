f0bafce0af2e8d390301a6fd0ddacd5a45fba1aa
Nicolas Skuček <nico.skucek@gmail.com>
2025-01-24 15:00:19 +0100

    Commented DBMigrationExtension.

--- src/MaintenanceChronicle.Api/Utils/DatabaseMigrationExtension.cs
+++ src/MaintenanceChronicle.Api/Utils/DatabaseMigrationExtension.cs
@@ -5,6 +5,12 @@ namespace MaintenanceChronicle.Api.Utils;
 
 public static class DatabaseMigrationExtension
 {
+    /// <summary>
+    /// Applies migrations to DB.
+    /// Asynchronous
+    /// </summary>
+    /// <param name="app">WebApplication on which you want the Db to be updated</param>
+    /// <returns>WebApplication</returns>
     public static async Task<WebApplication> ApplyMigrations(this WebApplication app)
     {
         using var scope = app.Services.CreateScope();
