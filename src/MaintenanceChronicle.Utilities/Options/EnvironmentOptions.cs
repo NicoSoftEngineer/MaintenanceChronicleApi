52773cb3ccd9d889c2bca460778dd446cbdc917a
Nicolas Skuček <nico.skucek@gmail.com>
2025-02-05 12:39:59 +0100

    Added password reset url property

--- src/MaintenanceChronicle.Utilities/Options/EnvironmentOptions.cs
+++ src/MaintenanceChronicle.Utilities/Options/EnvironmentOptions.cs
@@ -4,6 +4,7 @@ public class EnvironmentOptions
 {
     public required string FrontendHostUrl { get; set; }
     public required string FrontendConfirmUrl { get; set; }
+    public required string FrontendPasswordResetUrl { get; set; }
     public required string SenderEmail { get; set; }
     public required string SenderName { get; set; }
 }
