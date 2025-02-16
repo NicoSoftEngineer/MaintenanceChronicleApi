b8e30b6ccadfd6fcc3c9daa1449008809ec032df
Nicolas Skuček <nico.skucek@gmail.com>
2025-02-16 19:15:28 +0100

    Added checking mechanism to check if user has a tenant. If it has it adds it to the cookie

--- src/MaintenanceChronicle.Application/Validators/UserTenantValidationMiddleware.cs
+++ src/MaintenanceChronicle.Application/Validators/UserTenantValidationMiddleware.cs
@@ -1,19 +1,36 @@
 using System.Net;
+using MaintenanceChronicle.Application.Contracts.Tenants.Commands;
+using MaintenanceChronicle.Application.Contracts.Tenants.Commands.Dto;
+using MaintenanceChronicle.Application.Contracts.Users.Commands.Dto;
+using MaintenanceChronicle.Application.Contracts.Users.Queries;
 using MaintenanceChronicle.Data;
+using MaintenanceChronicle.Data.Entities.Account;
 using MaintenanceChronicle.Utilities.Enum;
 using MaintenanceChronicle.Utilities.Error;
 using MaintenanceChronicle.Utilities.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 
 namespace MaintenanceChronicle.Application.Validators;
 
-public class UserTenantValidationMiddleware(AppDbContext dbContext) : IMiddleware
+public class UserTenantValidationMiddleware(AppDbContext dbContext, IMediator mediator) : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         var userPrincipal = context.User;
         if (context.User.Identity!.IsAuthenticated)
         {
+            if (userPrincipal.GetTenantId() == null)
+            {
+                userPrincipal = await mediator.Send(new AddTenantClaimToUserPrincipalCommand(new UserTenantClaimDto
+                {
+                    Email = userPrincipal.GetUserEmail(),
+                    TenantId = await mediator.Send(new GetTenantIdFromUserCommand(userPrincipal.GetUserEmail()))
+                }, userPrincipal));
+                await context.SignInAsync(IdentityConstants.ApplicationScheme, userPrincipal);
+            }
             //if user has access to tenant continue
             if (!await dbContext.ValidateUserTenantAccess(userPrincipal.GetUserId(), userPrincipal.GetTenantId()))
             {
@@ -29,6 +46,7 @@ public class UserTenantValidationMiddleware(AppDbContext dbContext) : IMiddlewar
             }
         }
 
+        
         await next(context);
     }
 }
