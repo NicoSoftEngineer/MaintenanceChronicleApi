88fe914f776a4e2bd5d7ed62a6c2fc86ee39bdfe
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-13 19:26:01 +0100

    Added GeneratePasswordCreateEmail  command and handler

---
+++ src/MaintenanceChronicle.Application/Users/Commands/GeneratePasswordCreateEmailForUserCommandHandler.cs
@@ -0,0 +1,39 @@
+using MaintenanceChronicle.Application.Contracts.EmailMessages.Commands.Dto;
+using MaintenanceChronicle.Application.Contracts.Users.Commands;
+using MaintenanceChronicle.Data.Entities.Account;
+using MaintenanceChronicle.Utilities.EmailTemplates;
+using MaintenanceChronicle.Utilities.Error;
+using MaintenanceChronicle.Utilities.Options;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace MaintenanceChronicle.Application.Users.Commands;
+
+public class GeneratePasswordCreateEmailForUserCommandHandler(UserManager<User> userManager, IOptions<EnvironmentOptions> envOptions) : IRequestHandler<GeneratePasswordCreateEmailForUserCommand, NewEmailMessageDto>
+{
+    public async Task<NewEmailMessageDto> Handle(GeneratePasswordCreateEmailForUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByEmailAsync(request.Email);
+        if (user == null)
+        {
+            throw new BadRequestException(ErrorType.UserNotFound);
+        }
+
+        var passwordResetLink = $"{envOptions.Value.FrontendHostUrl}{envOptions.Value.FrontendPasswordCreateUrl.Replace("[Email]", user.Email).Replace("[PasswordToken]", request.PasswordToken).Replace("[ConfToken]", request.ConfToken)}";
+
+        var emailHelper = new EmailTemplateHelper();
+        var body = await emailHelper.GetUserInvitationEmailTemplate($"{user.FirstName} {user.LastName}", passwordResetLink);
+
+        var newEmailMessage = new NewEmailMessageDto
+        {
+            RecipientEmail = user.Email,
+            RecipientName = $"{user.FirstName} {user.LastName}",
+            Subject = "Email confirmation",
+            Body = body,
+        };
+
+        return newEmailMessage;
+    }
+}
