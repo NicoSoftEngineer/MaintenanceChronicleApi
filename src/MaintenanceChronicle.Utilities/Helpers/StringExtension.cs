05e71494a2b9219b4f06bfc383884159c4dd267d
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-17 22:55:52 +0100

    Renamed NormalizeStringExtension to StringExtenstion. Added Hash method for strings.

---
+++ src/MaintenanceChronicle.Utilities/Helpers/StringExtension.cs
@@ -0,0 +1,27 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MaintenanceChronicle.Utilities.Helpers;
+
+public static class StringExtension
+{
+    public static string NormalizeToUpper(this string value)
+    {
+        return new string(value.ToCharArray()
+            .Where(c => !Char.IsWhiteSpace(c))
+            .ToArray()!).ToUpperInvariant();
+    }
+
+    /// <summary>
+    /// Hashes input string using SHA256
+    /// </summary>
+    /// <param name="value">string to be hashed</param>
+    /// <returns>Hashed string</returns>
+    public static string Hash(this string value)
+    {
+        var bytes = Encoding.UTF8.GetBytes(value);
+        var hash = SHA256.HashData(bytes);
+        return Convert.ToBase64String(hash);
+    }
+}
+}
