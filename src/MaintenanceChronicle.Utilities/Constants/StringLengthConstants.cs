e8a3291ae8e28d65a2242200ced8ce7db570d7ff
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-26 19:58:04 +0100

    Added StringLengthConstants

---
+++ src/MaintenanceChronicle.Utilities/Constants/StringLengthConstants.cs
@@ -0,0 +1,10 @@
+namespace MaintenanceChronicle.Utilities.Constants;
+
+public class StringLengthConstants
+{
+    public const int MaxDescriptionLength = 2000;
+    public const int MaxEmailLength = 254;
+    public const int MaxNameLength = 255;
+    public const int MaxFirstNameLength = 50;
+    public const int MaxLastNameLength = 100;
+}
