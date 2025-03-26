9424e870f5da69d7e84f52c7146e024486a9081b
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-26 21:01:36 +0100

    Added SendAt prop to email message, to indicate when should the email be send. Added EmailBody max length

--- src/MaintenanceChronicle.Utilities/Constants/StringLengthConstants.cs
+++ src/MaintenanceChronicle.Utilities/Constants/StringLengthConstants.cs
@@ -3,6 +3,7 @@ namespace MaintenanceChronicle.Utilities.Constants;
 public class StringLengthConstants
 {
     public const int MaxDescriptionLength = 2000;
+    public const int MaxEmailBodyLength = 10000;
     public const int MaxEmailLength = 254;
     public const int MaxNameLength = 255;
     public const int MaxFirstNameLength = 50;
