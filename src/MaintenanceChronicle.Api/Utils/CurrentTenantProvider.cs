6063f7cd07a7a5ee8c96281df674cd3a4f6a0fea
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-28 15:49:23 +0100

    Updated CTP to not fail when the request is made by a background service

--- src/MaintenanceChronicle.Api/Utils/CurrentTenantProvider.cs
+++ src/MaintenanceChronicle.Api/Utils/CurrentTenantProvider.cs
@@ -7,8 +7,17 @@ namespace MaintenanceChronicle.Api.Utils;
 public class CurrentTenantProvider(IHttpContextAccessor httpContextAccessor)
     : ICurrentTenantProvider
 {
-    public Guid TenantId =>
-        httpContextAccessor.HttpContext!.User.FindFirstValue(MaintenanceChronicleClaimTypes.TenantIdClaimType) != null
-            ? new Guid(httpContextAccessor.HttpContext.User.FindFirstValue(MaintenanceChronicleClaimTypes.TenantIdClaimType)!)
-            : Guid.Empty;
+    public Guid TenantId => GetTenantId();
+       
+
+    public Guid GetTenantId()
+    {
+        if (httpContextAccessor.HttpContext != null)
+        {
+            return httpContextAccessor.HttpContext!.User.FindFirstValue(MaintenanceChronicleClaimTypes.TenantIdClaimType) != null
+                ? new Guid(httpContextAccessor.HttpContext.User.FindFirstValue(MaintenanceChronicleClaimTypes.TenantIdClaimType)!)
+                : Guid.Empty;
+        }
+        return Guid.Empty;
+    }
 }
