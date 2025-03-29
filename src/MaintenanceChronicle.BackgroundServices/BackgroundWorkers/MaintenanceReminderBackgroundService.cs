98429705945610a83c5e0ae3079148daac019145
Nicolas Skuček <nico.skucek@gmail.com>
2025-03-29 13:38:25 +0100

    Commented BackgroundServices project

--- src/MaintenanceChronicle.BackgroundServices/BackgroundWorkers/MaintenanceReminderBackgroundService.cs
+++ src/MaintenanceChronicle.BackgroundServices/BackgroundWorkers/MaintenanceReminderBackgroundService.cs
@@ -10,11 +10,21 @@ namespace MaintenanceChronicle.BackgroundServices.BackgroundWorkers;
 
 public class MaintenanceReminderBackgroundService(IServiceProvider provider) : BackgroundService
 {
+    /// <summary>
+    /// Function definition from BackgroundService, which gets called at the start of an app
+    /// Calls private CheckReminders
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
         await CheckReminders(cancellationToken);
     }
-
+    /// <summary>
+    /// Has infinite loop, gets all due reminders, generates email for each, and marks reminder as sent
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     private async Task CheckReminders(CancellationToken cancellationToken)
     {
         while (!cancellationToken.IsCancellationRequested)
